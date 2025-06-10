80944ac964f6cca2c9b916bff0719e8784f6af8c
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-06-10 22:35:28 +0100

    test: updates primary button to be generic with all button options

--- tests/unit/components/Button.test.tsx
+++ tests/unit/components/Button.test.tsx
@@ -1,18 +1,18 @@
 import { describe, expect, it, vi } from 'vitest';
-import PrimaryButton from '../../../src/components/PrimaryButton';
+import Button from '../../../src/components/Button';
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 
-describe('Given the PrimaryButton component', () => {
+describe('Given the Button component', () => {
   it('Then should find the element using button as role', async () => {
-    render(<PrimaryButton>Testing</PrimaryButton>);
+    render(<Button>Testing</Button>);
     const button = await screen.findByRole('button');
     expect(button).toBeDefined();
   });
 
   it('Then should the children passed as label', async () => {
     const children = 'any_text';
-    render(<PrimaryButton>{children}</PrimaryButton>);
+    render(<Button>{children}</Button>);
     const button = await screen.findByRole('button');
     expect(button).toHaveTextContent(children);
   });
@@ -20,7 +20,7 @@ describe('Given the PrimaryButton component', () => {
   describe('When the button is clicked', () => {
     it('Then should trigger onClick props function once', async () => {
       const mockFunction = vi.fn();
-      render(<PrimaryButton onClick={mockFunction}>Testing</PrimaryButton>);
+      render(<Button onClick={mockFunction}>Testing</Button>);
       const button = await screen.findByRole('button');
       await userEvent.click(button);
       expect(mockFunction).toHaveBeenCalledOnce();
